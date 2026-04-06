8f3c6c3bff10dda814e39fc98f50973f8e742802
Himadri Bhardwaj <himadribhardwaj56@gmail.com>
2026-04-06 23:19:44 +0530

    fix: add Vercel frontend URL to CORS

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin:[ 'http://localhost:3000','https://alumni-management-system.vercel.app']
     credentials: true
 }));
 app.use(express.json());
